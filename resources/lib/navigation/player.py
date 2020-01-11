6ecc414b8be8dcaca1e2fffb9e0da48f7eb85cfd
CastagnaIT <gottardo.stefano.83@gmail.com>
2020-01-11 17:54:57 +0100

    Use notification_time_offset for upnext

--- resources/lib/navigation/player.py
+++ resources/lib/navigation/player.py
@@ -176,8 +176,7 @@ def get_upnext_info(videoid, current_episode, metadata):
         next_info['play_info'] = {'play_path': common.build_url(videoid=next_episode_id,
                                                                 mode=g.MODE_PLAY)}
     if 'creditsOffset' in metadata[0]:
-        next_info['notification_time'] = (metadata[0]['runtime'] -
-                                          metadata[0]['creditsOffset'])
+        next_info['notification_time_offset'] = metadata[0]['creditsOffset']
     return next_info
 
 
